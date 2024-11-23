5eb7f5d7fd31a7701f8e206f521934ac3278b894
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-11-23 22:27:51 +0900

    impl post architects

--- src/app/api/architects/route.ts
+++ src/app/api/architects/route.ts
@@ -3,6 +3,12 @@ import { env } from '@/lib/env'
 import { loggerError, loggerInfo } from '@/lib/logger'
 import { notion } from '@/lib/notion'
 import { parseRequest } from '@/lib/utils'
+import {
+  heading2,
+  numberedListItem,
+  paragraph,
+} from '@sota1235/notion-sdk-js-helper/dist/blockObjects'
+import { richText } from '@sota1235/notion-sdk-js-helper/dist/richTextObject'
 import { type NextRequest, NextResponse } from 'next/server'
 import * as v from 'valibot'
 
@@ -17,16 +23,20 @@ export async function GET(req: NextRequest) {
 
 const BodySchema = v.object({
   name: v.string(),
+  overview: v.string(),
+  works: v.array(v.string()),
+  philosophy: v.string(),
+  externalLinks: v.array(v.object({ title: v.string(), url: v.string() })),
 })
 export async function POST(req: Request) {
   const data = await parseRequest(req, BodySchema)
   if (!data.success) {
-    return NextResponse.json({ message: 'Invalid body' }, { status: 400 })
+    return NextResponse.json(data.issues, { status: 400 })
   }
-  const { name } = data.output
+  const { name, overview, works, philosophy, externalLinks } = data.output
   return await notion.pages
     .create({
-      parent: { database_id: env.DAILY_DATABASE_ID },
+      parent: { database_id: env.ARCHITECT_DATABASE_ID },
       properties: {
         Name: {
           title: [
@@ -37,12 +47,19 @@ export async function POST(req: Request) {
             },
           ],
         },
-        Categories: {
-          select: {
-            name: '一言',
-          },
-        },
       },
+      children: [
+        heading2('概要'),
+        paragraph(overview),
+        heading2('代表作'),
+        ...works.map((work) => numberedListItem(work)),
+        heading2('建築哲学や特徴'),
+        paragraph(philosophy),
+        heading2('外部リンク'),
+        ...externalLinks.map((link, _index) =>
+          numberedListItem(richText(link.title, {}, link.url)),
+        ),
+      ],
     })
     .then((response) => {
       loggerInfo(`Created a page: ${JSON.stringify(response)}`, {

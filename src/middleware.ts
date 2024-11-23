31816d0414c0b18abc2f8cb8aec031cbe8437933
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-11-23 20:47:00 +0900

    delete cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,22 @@ import { env } from '@/lib/env'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const allowedOrigins = ['https://chatgpt.com']
-
 export function middleware(request: NextRequest) {
   const notionToken = request.headers.get('X-NOTION-TOKEN')
   if (notionToken === null || notionToken !== env.NOTION_TOKEN) {
     return new NextResponse('Unauthorized: Invalid API Key', { status: 401 })
   }
-  const origin = request.headers.get('origin')
-  if (origin && allowedOrigins.includes(origin)) {
-    const response = NextResponse.next()
-    response.headers.set('Access-Control-Allow-Origin', origin)
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    return response
-  }
-  return new NextResponse(`CORS policy does not allow access from ${origin}.`, { status: 403 })
+  // TODO: いずれCORSの設定を追加する
+  // const origin = request.headers.get('origin')
+  // if (origin && allowedOrigins.includes(origin)) {
+  //   const response = NextResponse.next()
+  //   response.headers.set('Access-Control-Allow-Origin', origin)
+  //   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  //   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  //   return response
+  // }
+  // return new NextResponse(`CORS policy does not allow access from ${origin}.`, { status: 403 })
+  return NextResponse.next()
 }
 
 export const config = {

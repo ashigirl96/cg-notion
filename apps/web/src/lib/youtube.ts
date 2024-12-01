6f136945c741bc3e69102e1b34e65039f897057a
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-12-02 08:32:37 +0900

    use other method

--- apps/web/src/lib/youtube.ts
+++ apps/web/src/lib/youtube.ts
@@ -1,5 +1,28 @@
-import { YoutubeTranscript } from 'youtube-transcript'
 import z from 'zod'
+
+import { Innertube } from 'youtubei.js/web'
+
+const youtube = await Innertube.create({
+  lang: 'en',
+  location: 'US',
+  retrieve_player: false,
+})
+
+const _fetchTranscript = async (url: string): Promise<string[]> => {
+  try {
+    const info = await youtube.getInfo(url)
+    const transcriptData = await info.getTranscript()
+    if (transcriptData?.transcript?.content?.body == null) {
+      throw new Error('Transcript not available')
+    }
+    return transcriptData.transcript.content.body.initial_segments.map(
+      (segment) => segment.snippet.text ?? '',
+    )
+  } catch (error) {
+    console.error('Error fetching transcript:', error)
+    throw error
+  }
+}
 
 export const transcriptSchema = z
   .string()
@@ -30,11 +53,10 @@ export const transcriptSchema = z
     const parsedUrl = new URL(url)
     // biome-ignore lint/style/noNonNullAssertion: <explanation>
     const videoId = new URLSearchParams(parsedUrl.search).get('v')!
-    const results = await YoutubeTranscript.fetchTranscript(videoId)
-    const result = results.map((r) => r.text).join('')
+    const result = (await _fetchTranscript(videoId)).join('')
 
     return `
-以下は、${videoId} の字幕の内容です。
+以下は、YoutubeのvideoId(${videoId}) の字幕の内容です。
 要約して、日本語でレスポンスしてください。
 
 ----
